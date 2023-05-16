93ef2a537e02307aabc2bd63cd12647ae545aa09
rayaneRX <elkerayane@gmail.com>
2023-05-16 05:41:14 -0700

    task 0

---
+++ module3_task0/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update && apt-get install -y make curl
+curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
+rm hugo_extended_0.84.0_Linux-64bit.tar.gz
+export PATH=/usr/local/bin:$PATH 
+make build

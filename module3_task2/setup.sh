d41b02e1cced3293500b0f671555a5042d9346ca
rayaneRX <elkerayane@gmail.com>
2023-05-23 07:41:25 -0700

    six the setup

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-apt-get update && apt-get install -y make curl
+apt-get update && apt-get install -y make curl npm
 curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
 rm hugo_extended_0.84.0_Linux-64bit.tar.gz
 export PATH=/usr/local/bin:$PATH 
+sudo npm install -g markdownlint-cli
 make build
